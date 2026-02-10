5f3697b9619d516b03caaab156d6def73630f3e7
donggyun-han <hdg5639@daum.net>
2026-02-10 19:34:58 +0900

    chore: 프로젝트 파일 생성

---
+++ MemoryMe/src/main/java/memme/memoryme/MemoryMeApplication.java
@@ -0,0 +1,13 @@
+package memme.memoryme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryMeApplication.class, args);
+    }
+
+}
