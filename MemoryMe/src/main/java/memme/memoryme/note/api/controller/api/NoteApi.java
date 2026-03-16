c86693feec890038776b8086a64d75139ce51f24
donggyun-han <hdg5639@daum.net>
2026-03-17 04:44:29 +0900

    feat(note, global): 노트 컨트롤러 추가 및 공통 응답 래퍼 개선 - NoteController 생성 - 공통 응답 래퍼를 class에서 record로 변경 - 응답 생성용 정적 팩토리 메서드 추가

--- MemoryMe/src/main/java/memme/memoryme/note/api/controller/api/NoteApi.java
+++ MemoryMe/src/main/java/memme/memoryme/note/api/controller/api/NoteApi.java
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "Note API", description = "메모 API")
-@RequestMapping("/v1/event")
+@RequestMapping("/v1")
 public interface NoteApi {
 }
