ff6268e3e1e5f7df8c2e016928b91da387c4f7b3
donggyun-han <hdg5639@daum.net>
2026-04-06 23:34:21 +0900

    docs(swagger): swagger prod url 수정

--- MemoryMe/src/main/java/memme/memoryme/note/api/dto/note/NewNoteDto.java
+++ MemoryMe/src/main/java/memme/memoryme/note/api/dto/note/NewNoteDto.java
@@ -1,9 +1,13 @@
 package memme.memoryme.note.api.dto.note;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "메모 생성 DTO")
 public record NewNoteDto(
-        // 추후 삭제
+        // todo: 추후 삭제
         Long userId,
 
+        @Schema(description = "내용", example = "오늘 회의 정리")
         String title
 ) {
 }
